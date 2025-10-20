118e9ad13b7607c9cedf3eccfe2c1a26796f3df9
BoutroisJordan <jordanboutroispro@gmail.com>
2025-10-20 22:35:39 +0200

    feat(login): update layout login

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -12,18 +12,15 @@ export default function AuthLayout({
       <div className="  auth-side md:w-1/2 items-center justify-center">
         <div className="text-center">
           {/* Replace with your actual logo component or image */}
-            <Logo theme="light" variant={3} />
+          <Logo theme="light" variant={3} />
 
-          <h1 className="text-white text-2xl font-semibold">Your App Name</h1>
-          <p className="text-gray-400 mt-2">Back-office Management</p>
+          <h1 className="text-white text-2xl font-semibold">BACK-OFFICE</h1>
         </div>
       </div>
 
       {/* Right Side - Dynamic Content (prend tout l'écran sur mobile) */}
       <div className="flex-1 flex items-center justify-center bg-white p-8">
-        <div className="w-full max-w-md">
-          {children}
-        </div>
+        <div className="w-full max-w-md">{children}</div>
       </div>
     </div>
   );
