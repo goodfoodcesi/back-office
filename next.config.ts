4fa742006537e383c41b34e8e03fab1d98379af7
BoutroisJordan <jordanboutroispro@gmail.com>
2025-10-24 18:20:55 +0200

    fix(config): fix other path config for kube

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,26 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "standalone",
-  basePath: '/bo'
+  basePath: '/bo',
+
+  assetPrefix: '/bo',
+  // Configuration des images
+  images: {
+    path: '/bo/_next/image',
+    unoptimized: false,
+  },
+
+
+  async redirects() {
+    return [
+      {
+        source: '/login',
+        destination: '/bo/login',  // ← Avec basePath
+        permanent: false,
+      },
+    ]
+  },
+
 };
 
 export default nextConfig;
