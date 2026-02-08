7f2eacb83391f258a62fb9ef4ef27562c8ea8fe9
BoutroisJordan <jordanboutroispro@gmail.com>
2026-02-08 16:39:24 +0100

    fix var env url

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -3,7 +3,7 @@ import { nextCookies } from "better-auth/next-js";
 import { inferAdditionalFields } from "better-auth/client/plugins";
 
 export const authClient =  createAuthClient({
-    baseURL: process.env.NEXT_PUBLIC_AUTH_BASE_URL ?? "http://localhost:4000",
+    baseURL: process.env.NEXT_PUBLIC_AUTH_BASE_URL,
     plugins: [
         nextCookies(),
         inferAdditionalFields({
